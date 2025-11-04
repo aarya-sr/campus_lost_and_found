2e476c99fc409d78349f1efef3000dce752cb1c8
Aarya <aaryasrivastava2007@gmail.com>
2025-11-05 01:08:54 +0530

    Initial commit: backend setup with MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+
+dotenv.config();
+const app = express();
+app.get('/',(req,res)=>{
+    res.send('Server is ready');
+})
+
+app.get('/products',(req,res)=>{
+    
+})
+
+console.log(process.env.MONGO_URI);
+
+app.listen(3000, () => {
+    connectDB();
+ 
+    console.log('Server is running on port 3000');
+    });
