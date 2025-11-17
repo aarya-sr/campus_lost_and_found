0431190f98213d8a456613624a842155c8f09cc2
Aarya <aaryasrivastava2007@gmail.com>
2025-11-17 15:01:02 +0530

    Signup done

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,34 @@
+export default function Signup(){
+    return(
+        <div className="flex justify-center items-center h-screen bg-gray-100 px-4">
+        <div className="flex justify-center items-center h-screen bg-gray-100 px-4">
+            <h2 className="text-3xl font-bold text-center mb-6">Signup</h2>
+
+            <form>
+                <input
+                type="text"
+                placeholder="Full Name"
+                className="w-full p-3 border rounded-xl mb-4 outline-none focus:ring-2 focus:ring-blue-500"
+                />
+
+                <input
+                type="email"
+                placeholder="Email"
+                className="w-full p-3 border rounded-xl mb-4 outline-none focus:ring-2 focus:ring-blue-500"
+                />
+
+                <input
+                type="password"
+                placeholder="Password"
+                className="w-full p-3 border rounded-xl mb-6 outline-none focus:ring-2 focus:ring-blue-500"
+                />
+
+                <button type="submit"
+                className="w-full bg-blue-600 text-white p-3 rounded-xl hover:bg-blue-700 transition"
+                >Signup
+                </button>
+            </form>
+        </div>
+        </div>
+    )
+}
